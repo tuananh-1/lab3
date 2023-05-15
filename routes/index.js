7128b17f1219162f3772814dd085ca8e9a8c8ba4
tuananh-1 <anhldtgdh210341@fpt.edu.vn>
2023-05-15 17:12:00 +0700

    com

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,21 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', (req, res) => {
+  res.render('home');
+})
+
+router.get('/football', (req, res) =>
+ {let manu = "MU";
+  let chelsea = "chelsea";
+  let mancity = "MC";
+  let arsenal = "ars";
+  res.render('bongda', { manu: manu, chelsea: chelsea, arsenal:arsenal, mancity: mancity});
 });
+
+router.get('/interests', (req, res) => {
+  var interest = ["money","music","game"];
+  res.render('interest', { interest: interest});
+})
 
 module.exports = router;
